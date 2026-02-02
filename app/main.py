f1709b98912f045a77d9564a735cf7d7c1b22e5e
K N <niedbalaproject@gmail.com>
2026-02-02 15:38:03 +0100

    Add Person class with registry

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
